465f3618623aedc86361b0e2487b71e44e716365
yatin <yatingoyal31@gmail.com>
2023-01-28 15:59:16 +0530

    bcrypt portion added

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,13 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+function isStringInvalid(s){
+
+   if(s.length === 0 ||s == null ){
+     return true;
+   }
+   return false;
+}
 
 exports.postUser = (req,res,next)=>{
     
@@ -6,46 +15,66 @@ exports.postUser = (req,res,next)=>{
     const email = req.body.email;
     const password = req.body.password;
 
-    if(user == null || user.length === 0|| email == null || email.length === 0|| password.length === 0||password == null){
-      return res.status(400).json({error : "Bad Parameters . Something is Missing"});
-    }
-
-    User.create({
-       name:user,
-       email:email,
-       password: password
-    })
-   .then((results) => {
-    res.status(201).json({message: "Successfully created new user"})
+   //  if(user == null || user.length === 0|| email == null || email.length === 0|| password.length === 0||password == null){
+   //    return res.status(400).json({error : "Bad Parameters . Something is Missing"});
+   //  }
+  
+   try{
+      if(isStringInvalid(user) || isStringInvalid(email) || isStringInvalid(password)){
+         return res.json(400).json({error: "Bad Parameters . Something is Missing"});
+       }
+   
+       bcrypt.hash(password,10, async (err,hash) => {
+         await User.create({
+            name:user,
+            email:email,
+            password: hash
+         })
+         res.status(201).json({message: "Successfully created new user"})
    })
-   .catch(err => {
-   res.status(500).json({message: "user already exist"});
-   });
+   }
+     catch(err){
+      res.status(500).json({message: "user already exist"});
+      };
+
 };
 
-exports.checkUser = (req,res,next) => {
+exports.checkUser = async (req,res,next) => {
      
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findOne({ where: { email:email } })
-    .then((user) => {
-      
-      if(user !== null && user.dataValues.password === password){
-         res.status(201).json(user);
+    try{
+      if(isStringInvalid(email) || isStringInvalid(password)){
+         return res.json(400).json({message: "Bad Parameters . Something is Missing",success: false});
+       }
+   
+       const user = await User.findOne({ where: { email:email } })
+         
+         if(user !== null ){
 
-      }
-      else if(user === null){
-         res.status(404).json({message:"user not found"});
-      }
-      else if(user.dataValues.password != password){
-         res.status(401).json({message:"user not authorized"});
-      }
+            bcrypt.compare(password ,user.dataValues.password,(err,result)=>{
 
-    })
-    .catch((err)=>{
+               if(err){
+                  // return res.status(500).json({success:true, message:"something went wrong"});
+                  throw new Error("Something Went Wrong");
+               }
+               if(result === true){
+                  return res.status(201).json({message:"user logged in successfully", success:true});
+               }
+               else{
+                  return res.status(401).json({message:"user not authorized" , success: false});
+               }
+            })
+        }
+         else {
+            return res.status(404).json({message:"user not found" , success: false});
+         }
+   
+    }
+    catch(err){
       console.log(err);
-       res.status(404).json(err);
-    })
+       return res.status(500).json(err);
+    }
 
 };
