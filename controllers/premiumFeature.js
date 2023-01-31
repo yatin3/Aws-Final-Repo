c749170083786a38169e3d077cb3481ea9a799f8
yatin <yatingoyal31@gmail.com>
2023-01-31 13:52:58 +0530

    leaderboard optimisation added

--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -2,35 +2,119 @@ const User = require('../models/user');
 const Expense = require('../models/expense');
 const sequelize = require('../util/database');
 
-exports. getUserLeaderBoard = async (req,res) =>{
-    try{
-       const users = await User.findAll();
-       const expenses = await Expense.findAll();
-       const userAggregatedExpenses = {};
+// exports. getUserLeaderBoard = async (req,res) =>{
+//     try{
+//        const users = await User.findAll();
+//        const expenses = await Expense.findAll();
+//        const userAggregatedExpenses = {};
 
-     //  console.log(expenses);
-       expenses.forEach((expense)=>{
+//        expenses.forEach((expense)=>{
        
-      //  const newId = expense.UserId;
-       // console.log(userAggregatedExpenses.newId);
-        if(userAggregatedExpenses[expense.UserId]){
-            userAggregatedExpenses[expense.UserId] =   userAggregatedExpenses[expense.UserId] +Number(expense.amount);
-        }else{
-            userAggregatedExpenses[expense.UserId] = Number(expense.amount);
-        }
-       })
+//         if(userAggregatedExpenses[expense.UserId]){
+//             userAggregatedExpenses[expense.UserId] =   userAggregatedExpenses[expense.UserId] +Number(expense.amount);
+//         }else{
+//             userAggregatedExpenses[expense.UserId] = Number(expense.amount);
+//         }
+//        })
       
-       let userLeaderBoardDetails = [];
-       users.forEach((user)=>{
-        userLeaderBoardDetails.push({name:user.name, total_cost:userAggregatedExpenses[user.id]||0});
-       })
-      // console.log(userAggregatedExpenses);
-      userLeaderBoardDetails.sort((a,b)=> b.total_cost-a.total_cost)
-       res.status(200).json(userLeaderBoardDetails);
-    }
+//        let userLeaderBoardDetails = [];
+//        users.forEach((user)=>{
+//         userLeaderBoardDetails.push({name:user.name, total_cost:userAggregatedExpenses[user.id]||0});
+//        })
+//       userLeaderBoardDetails.sort((a,b)=> b.total_cost-a.total_cost)
+//        res.status(200).json(userLeaderBoardDetails);
+//     }
+//     catch(err){
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+// }
+
+// exports. getUserLeaderBoard = async (req,res) =>{
+//     try{
+//        const users = await User.findAll({
+//          attributes: ['id','name']
+//        });
+//        const expenses = await Expense.findAll({
+//          attributes: ['UserId','amount']
+//        });
+//        const userAggregatedExpenses = {};
+
+//        expenses.forEach((expense)=>{
+       
+//         if(userAggregatedExpenses[expense.UserId]){
+//             userAggregatedExpenses[expense.UserId] =   userAggregatedExpenses[expense.UserId] +Number(expense.amount);
+//         }else{
+//             userAggregatedExpenses[expense.UserId] = Number(expense.amount);
+//         }
+//        })
+      
+//        let userLeaderBoardDetails = [];
+//        users.forEach((user)=>{
+//         userLeaderBoardDetails.push({name:user.name, total_cost:userAggregatedExpenses[user.id]||0});
+//        })
+//       userLeaderBoardDetails.sort((a,b)=> b.total_cost-a.total_cost)
+//        res.status(200).json(userLeaderBoardDetails);
+//     }
+//     catch(err){
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+// }
+
+// exports. getUserLeaderBoard = async (req,res) =>{
+//   const expenses = await Expense.findAll();
+//     try{
+//        const leaderBoardOfUser = await User.findAll({
+//     //    attributes: ['id','name',[sequelize.fn('sum', sequelize.col('expenses.amount')),'total_cost']],
+//         attributes: ['id','name','expenses.amount'],
+       
+//        include: [
+//         {
+//           model:Expense,
+//           attributes:['id','amount','category']
+//         }
+//        ],
+
+//       // group: ['User.id']
+
+//       });
+//       console.log(leaderBoardOfUser);
+//       res.status(200).json(leaderBoardOfUser);
+//       }
+
+//     catch(err){
+//       console.log(err);
+//       res.status(500).json(err);
+    
+// }
+
+exports. getUserLeaderBoard = async (req,res) =>{
+  
+    try{
+       const leaderBoardOfUser = await User.findAll({
+        attributes: ['id','name',[sequelize.fn('sum', sequelize.col('expenses.amount')),'total_cost']],
+       
+       include: [
+        {
+          model:Expense,
+          attributes:[]
+        }
+       ],
+
+       group: ['User.id'],
+       order:[['total_cost',"DESC"]]
+
+      });
+      console.log(leaderBoardOfUser);
+      res.status(200).json(leaderBoardOfUser);
+      }
+
     catch(err){
       console.log(err);
       res.status(500).json(err);
-    }
+    
+}
+
 }
 
